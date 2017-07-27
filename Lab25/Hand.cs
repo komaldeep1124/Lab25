a4a30630dc860f41011e68f80971f82081effee6
KoMaL <KoMaL@LAPTOP-AOK7R0HH>
2017-07-27 18:32:07 -0400

    added hand class

---
+++ Lab25/Hand.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab25
+{
+   public  class Hand : CardList
+    {
+        // PRIVATE INSTANCE VARIABLES
+ 
+         // PUBLIC PROPERTIES
+ 
+         // CONSTRUCTORS
+ 
+         // PRIVATE METHODS
+         protected override void _initialize()
+         {
+             // STUB
+           // this method is empty
+       }
+ 
+         // PUBLIC METHODS
+ 
+         /// <summary>
+         /// This method overrides the built-in ToString method.
+         /// </summary>
+         /// <returns>
+         /// This method returns the current cards in the deck
+         /// </returns>
+         public override string ToString()
+         {
+             string outputString = "";
+ 
+             outputString += "Hand Contains  Number of Cards: " + this.Count + "\n";
+             outputString += "==================================\n";
+ 
+            foreach (Card card in this)
+            {
+                 outputString += "The " + card.Face + " of " + card.Suit + "\n";
+             }
+ 
+            return outputString;
+    }
+}
+}
